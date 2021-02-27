f11b8befab124a78ceb355656392509edd1adcc9
Omair Muhi <omairmuhi@icloud.com>
2021-02-27 17:28:23 -0500

    Subject: Add Sort button to App Problem: NA Solution: NA Note: NA

--- emp-dir-app/src/App.js
+++ emp-dir-app/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
-// import EmployeeCard from './components/EmployeeCard';
 import Wrapper from './components/Wrapper';
 import Title from './components/Title';
 import Table from './components/Table';
 import employees from './employees.json';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isSorted: false }
+        this.handleSortClick = this.handleSortClick.bind(this);
+    }
+    handleSortClick() {
+        this.setState(state => ({
+            isSorted: !state.isSorted
+        }));
+    }
     render() {
         return (
             <Wrapper>
                 <Title>Employee Directory</Title>
+                <button type="button" className="btn btn-primary mb-3" onClick={this.handleSortClick}>
+                    {this.state.isSorted ? 'Show' : 'Sort'}
+                </button>
                 <Table employeesArr={employees}></Table>
             </Wrapper >
         );
