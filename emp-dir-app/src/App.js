3a7b004c3e43e6adca203129da3f30bba9be2df0
Omair Muhi <omairmuhi@icloud.com>
2021-02-27 17:43:40 -0500

    Subject: Add filter button Problem: NA Solution: NA Note: NA

--- emp-dir-app/src/App.js
+++ emp-dir-app/src/App.js
@@ -2,26 +2,41 @@ import React from 'react';
 import Wrapper from './components/Wrapper';
 import Title from './components/Title';
 import Table from './components/Table';
+// import ButtonGroup from './components/ButtonGroup';
 import employees from './employees.json';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { isSorted: false }
+        this.state = {
+            isSorted: false,
+            isFiltered: false
+        }
         this.handleSortClick = this.handleSortClick.bind(this);
+        this.handleFilterClick = this.handleFilterClick.bind(this);
     }
     handleSortClick() {
         this.setState(state => ({
             isSorted: !state.isSorted
         }));
     }
+    handleFilterClick() {
+        this.setState(state => ({
+            isFiltered: !state.isFiltered
+        }));
+    }
     render() {
         return (
             <Wrapper>
                 <Title>Employee Directory</Title>
-                <button type="button" className="btn btn-primary mb-3" onClick={this.handleSortClick}>
-                    {this.state.isSorted ? 'Show' : 'Sort'}
-                </button>
+                <div className="btn-group mb-3">
+                    <button type="button" className="btn btn-primary btn-outline-dark" onClick={this.handleSortClick}>
+                        {this.state.isSorted ? 'Show' : 'Sort'}
+                    </button>
+                    <button type="button" className="btn btn-primary btn-outline-dark" onClick={this.handleFilterClick}>
+                        {this.state.isFiltered ? 'Show' : 'Filter'}
+                    </button>
+                </div>
                 <Table employeesArr={employees}></Table>
             </Wrapper >
         );
